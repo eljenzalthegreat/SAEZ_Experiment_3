905878f4484ffbd727656121b114593dcc757088
eljenzalthegreat <hopersaez@gmail.com>
2024-09-17 02:09:22 +0800

    Add files via upload

---
+++ SAEZ_Pandas-P1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### SAEZ, Eljenzal Hoper U. 
+
+# #### 2ECE - C
+
+# ### Problem 1
+
+# In[30]:
+
+
+# import the necessary library
+import pandas as pd
+
+# define the file path 
+file = 'cars.csv'  # Replace with your file path
+
+# load the CSV file into a DataFrame 
+cars = pd.read_csv(file_path)
+
+# display the first five rows of the DataFrame
+print("First five rows of the DataFrame:")
+print(cars.head())
+
+# display the last five rows of the DataFrame
+print("\nLast five rows of the DataFrame:")
+print(cars.tail())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
